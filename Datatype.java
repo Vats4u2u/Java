5d796c7f6652f9652bed09c7c874dda257458e93
Vats4u2u <pranshuvats232@gmail.com>
2024-05-22 17:01:46 +0530

    First

---
+++ Datatype.java
@@ -0,0 +1,20 @@
+class Datatype{
+    public static void main(String a[])
+    {
+    //integers
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l =8545l;
+    
+    //Float
+        float f = 5.8f;
+        double d = 5.8;
+
+    //character
+        char c = '8';
+
+    //boolean
+        boolean b = true;
+    }
+}
---
+++ Literal.java
@@ -0,0 +1,9 @@
+class Literal{
+    public static void main(String a[])
+    {
+        //Literal
+
+        float num1 = 56f;
+        System.out.println(num1);
+    }
+}
