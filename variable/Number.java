727f8b70b497020e12b7c617aeb5cced6aacf069
Vats4u2u <pranshuvats232@gmail.com>
2024-05-27 17:11:29 +0530

    okay

---
+++ variable/Number.java
@@ -0,0 +1,7 @@
+class Number{
+    public static void main(String[] args){
+        int myNum = 15;
+        myNum = 50; //Now myNum 50//
+        System.out.println(myNum);
+    }
+}
---
+++ variable/string.java
@@ -0,0 +1,7 @@
+class string{
+    public static void main(String[] args){
+        
+        String name = "Priyanshu";
+        System.out.print(name);
+    }
+}
